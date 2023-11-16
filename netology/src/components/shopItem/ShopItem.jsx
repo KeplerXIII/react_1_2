9dde412309939b3f41042d660a250191320b1389
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2023-11-16 15:21:47 +0300

    Completed

--- netology/src/components/shopItem/ShopItem.jsx
+++ netology/src/components/shopItem/ShopItem.jsx
@@ -1,22 +1,28 @@
 /* eslint-disable react/prop-types */
+import React from 'react'
 import './ShopItem.css'
 
-export default function ShopItemFunc (props) {
-    const item = props.item
+export default class ShopItemFunc extends React.Component {
+  constructor({props}) {
+    super(props)
+  }
+  render() {
+    const { item } = this.props 
     return (
-    <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-    <div className="description">
-        {item.descriptionFull}
-    </div>
-    <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-    <div className="divider"></div>
-    <div className="purchase-info">
-      <div className="price">{item.currency}{item.price.toFixed(2)}</div>
-      <button>Добавить в корзину</button>
-    </div>
-  </div>
-  )
+      <div className="main-content">
+          <h2>{item.brand}</h2>
+          <h1>{item.title}</h1>
+          <h3>{item.description}</h3>
+        <div className="description">
+          {item.descriptionFull}
+        </div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    )
+  }
 }
